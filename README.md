8198ef89337e7495cfe13570b95b7162b1d6a01f
mahendra-wts <mahendra.v@wingsts.com>
2026-05-25 19:17:54 +0530

    feat: implement react-router-dom with layout components and configure code styling

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # DevTinder
 
+S2E15:
 - Create a Vite + React application project setup
 - Remove unneccessary files
 - Installed tailwind css
 - Installed daisyUI Tailwind css component library
-- Added Navbar code to app.jsx
+- Created separate NavBar component and imported to Body.jsx
+- Installed react-router-dom for routing 
+- Created BrowserRouter > Routes > Route=/ Body > RouteChildren
+- Created an Outlet in your Body.jsx to render the RouteChildren like login and profile
+- Created separate Footer component and imported to Body.jsx
--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,13 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js"
+import globals from "globals"
+import reactHooks from "eslint-plugin-react-hooks"
+import reactRefresh from "eslint-plugin-react-refresh"
+import { defineConfig, globalIgnores } from "eslint/config"
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{js,jsx}'],
+    files: ["**/*.{js,jsx}"],
     extends: [
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
@@ -16,6 +16,10 @@ export default defineConfig([
     languageOptions: {
       globals: globals.browser,
       parserOptions: { ecmaFeatures: { jsx: true } },
+    },
+    rules: {
+      "quotes": ["error", "double"],
+      "jsx-quotes": ["error", "prefer-double"],
     },
   },
 ])
