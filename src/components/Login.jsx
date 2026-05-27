61774c6ba410e62f2a67e6c0fd0471506cf54158
mahendra-wts <mahendra.v@wingsts.com>
2026-05-27 19:51:44 +0530

    feat: add password visibility toggle and form validation with error messaging to login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -8,12 +8,17 @@ import { BASE_URL } from "../utils/constants";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+  const [errorMessage, setErrorMessage] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const handleLogin = async () => {
     try {
+      if (!email || !password) {
+        setErrorMessage("Please fill all the fields");
+        return;
+      }
       const res = await axios.post(
         BASE_URL + "/login",
         {
@@ -29,7 +34,12 @@ const Login = () => {
       navigate("/");
     } catch (error) {
       console.log(error);
+      setErrorMessage(error?.response?.data?.message || "Something went wrong");
     }
+  };
+
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
   };
 
   return (
@@ -130,7 +140,7 @@ const Login = () => {
                   </svg>
                 </span>
                 <input
-                  type="password"
+                  type={showPassword ? "text" : "password"}
                   placeholder="••••••••"
                   value={password}
                   className="input input-bordered w-full pl-10 pr-10 bg-slate-950/40 border-slate-800 text-slate-200 placeholder-slate-600 focus:outline-none focus:ring-2 focus:ring-purple-500/20 focus:border-purple-500 transition-all duration-200"
@@ -140,41 +150,64 @@ const Login = () => {
                 />
                 <button
                   type="button"
+                  onClick={handleShowPassword}
                   className="absolute inset-y-0 right-0 flex items-center pr-3 text-slate-500 hover:text-slate-300 focus:outline-none"
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={2}
-                    stroke="currentColor"
-                    className="w-4.5 h-4.5"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
-                    />
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                    />
-                  </svg>
+                  {showPassword ? (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={2}
+                      stroke="currentColor"
+                      className="w-4.5 h-4.5"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={2}
+                      stroke="currentColor"
+                      className="w-4.5 h-4.5"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                      />
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                      />
+                    </svg>
+                  )}
                 </button>
               </div>
             </div>
 
+            {/* Error Message */}
+            {errorMessage && (
+              <p className="text-sm text-red-500">{`Error : ${errorMessage}`}</p>
+            )}
+
             {/* SUBMIT BUTTON */}
             <button
-              className="btn w-full mt-6 bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 text-white font-bold text-sm tracking-wider uppercase border-none hover:shadow-[0_0_20px_rgba(236,72,153,0.4)] transition-all duration-300 transform active:scale-[0.98] py-4 h-auto min-h-0 rounded-2xl"
+              className="btn w-full mt-6 bg-linear-to-r from-pink-500 via-purple-500 to-indigo-500 text-white font-bold text-sm tracking-wider uppercase border-none hover:shadow-[0_0_20px_rgba(236,72,153,0.4)] transition-all duration-300 transform active:scale-[0.98] py-4 h-auto min-h-0 rounded-2xl"
               onClick={(e) => {
                 e.preventDefault();
                 handleLogin();
               }}
             >
               <span className="flex items-center justify-center gap-1.5">
-                <span>Authorize Session</span>
+                <span>Login</span>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className="h-4.5 w-4.5"
