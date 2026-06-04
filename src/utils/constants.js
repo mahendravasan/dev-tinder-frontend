b8813749e10129fdbb16e56ce6a6fdb194099594
mahendra-wts <mahendra.v@wingsts.com>
2026-06-04 17:56:23 +0530

    Update BASE_URL to dynamically switch between localhost and production API endpoints

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,4 @@
 // export const BASE_URL = "http://localhost:1300";
-export const BASE_URL = "/api";
+// export const BASE_URL = "/api";
+export const BASE_URL =
+  location.hostname === "localhost" ? "http://localhost:1300" : "/api";
